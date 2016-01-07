47877f850e6bb5f39bdb78477f339a617677f75c
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-01-07 15:07:33 +0100

    Improved formatting of help text

--- readStudyLog.py
+++ readStudyLog.py
@@ -3,17 +3,16 @@ import sys
 
 def printHelp():
 	scriptname = sys.argv[0].replace('./', '').replace('.\\', '')
-	print("Usage:")
-	print(scriptname + " <option>")
-	print("    This script parses the study logfile for human readability.")
-	print("    Options:")
-	print("        -f [tag]")
-	print("            Prints out all the lines in the logfile of type [tag].")
+	print("Usage: " + scriptname + " <option> [arguments]")
+	print("This script parses the study logfile for human readability.")
+	print()
+	print("Options:")
+	print("    -f, --filter <tag>  Prints out all the lines in the logfile of type <tag>.")
 
 needHelp = True
 if len(sys.argv) > 2:
 	option = sys.argv[1]
-	if option == '-f' and len(sys.argv) >= 3:
+	if (option == '-f' or option == '--filter') and len(sys.argv) >= 3:
 		tag = sys.argv[2]
 		filteredLines = []
 		if len(tag) > 0:
