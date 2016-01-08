1515cef6664daa40a88ea92e6012416a8e1cede7
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-01-08 02:18:03 +0100

    Added -p option and an unimplemented -h, --help option to study.py

--- study.py
+++ study.py
@@ -2,6 +2,9 @@ import time
 import winsound
 import sys
 
+studyTime = 20 * 60
+pauseTime = 5 * 60
+
 
 def timeStr():
 	return time.strftime("%H:%M:%S")
@@ -12,33 +15,48 @@ def writeToLog(p, t, c):
 	logfile.write(p + ' ' + t + ' ' + c + '\n')
 	logfile.close()
 
+
+def pause():
+	print("paus! " + str(pauseTime / 60) + " min")
+	writeToLog('[f]', timeStr(), 'pause started')
+	time.sleep(pauseTime)
+	winsound.Beep(500, 550)
+	input("återgå till plugget?")
+	writeToLog('[t]', timeStr(), 'pause ended')
+
 studying = False
 
 
 def main():
 	global studying
-	pluggtid = 20 * 60
-	paustid = 5 * 60
+	intialPause = False
+
+	if len(sys.argv) > 1:
+		option = sys.argv[1]
+
+		if option == '-p':
+			intialPause = True
+		elif option == '-h' or '--help':
+			print("Implement: write help statement.")
+			return
+
 	writeToLog('[s]', timeStr(), 'session started')
+	if intialPause:
+		pause()
 	while True:
 		# start studying
-		print(timeStr() + " plugga! " + str(pluggtid / 60) + " min")
+		print(timeStr() + " plugga! " + str(studyTime / 60) + " min")
 		writeToLog('[d]', timeStr(), 'studying started')
 		studying = True
 		winsound.Beep(600, 550)
-		time.sleep(pluggtid)
+		time.sleep(studyTime)
 		winsound.Beep(400, 1000)
-		comment = input("i de senaste " + str(pluggtid / 60) + " min har jag: ")
+		comment = input("i de senaste " + str(studyTime / 60) + " min har jag: ")
 		writeToLog('[r]', timeStr(), comment)
 		studying = False
 
 		# take break
-		print("paus! " + str(paustid / 60) + " min")
-		writeToLog('[f]', timeStr(), 'pause started')
-		time.sleep(paustid)
-		winsound.Beep(500, 550)
-		input("återgå till plugget?")
-		writeToLog('[t]', timeStr(), 'pause ended')
+		pause()
 
 if __name__ == '__main__':
 	try:
